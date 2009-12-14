3b1f1482585b6b64ed9f9ec06ed64e6edab61ee7
Dianne Hackborn <hackbod@google.com>
2009-12-14 10:49:42 -0800

    sm6115-common: Add location related features for GPS enabled targets
    Change-Id: Ibecf665cb21b89fef09951ece379f9f53353698f

--- common.mk
+++ common.mk
@@ -196,6 +196,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti
