556a757df1bb213fef8b5d1fc3ed51f238aeba4d
Alex Wong <waiw@codeaurora.org>
2015-06-06 10:49:33 -0700

    sm6115-common: Add AEP support config file
    This change exposes the support for Android extension pack (AEP) in the
    graphics driver by copying the android.hardware.opengles.aep.xml to the
    /system/etc/permissions directory on the device.
    
    Change-Id: I87a696cc35356d7134b41468e0ad549ee95b0dbd

--- common.mk
+++ common.mk
@@ -23,6 +23,7 @@ PRODUCT_PACKAGES += \
 
 # Adreno
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # Additional native libraries
