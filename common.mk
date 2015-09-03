65ea7f1d621949a0a2fd9350a8845344546209b3
Mayank Rana <mrana@codeaurora.org>
2015-09-03 16:10:07 -0700

    sm6115-common: Enable MIDI feature
    MIDI feature is disable currently. This change enables the same
    so required components are compiled and accesible.
    
    Change-Id: I80d3115e0618d9bb6552ee599b4a82d583d62b24

--- common.mk
+++ common.mk
@@ -80,7 +80,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
-    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
+    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
