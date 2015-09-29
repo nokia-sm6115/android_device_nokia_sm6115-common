c4e9aa7f58489ff3424050a1bde5133141b549f0
Etan Cohen <etancohen@google.com>
2015-09-29 13:48:43 -0700

    sm6115-common: Declare support for WiFi Aware
    Change-Id: I1ef8fc650130d3510f73d7a4233e60a037fad8eb

--- common.mk
+++ common.mk
@@ -405,6 +405,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # WiFi Display
