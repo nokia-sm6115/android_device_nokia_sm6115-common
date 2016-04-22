a4ffccd408f7b8e9bdf7dc51ce314abe79216f3a
Alex Deymo <deymo@google.com>
2016-04-21 17:30:43 -0700

    sm6115-common: Build A/B update engine
    Change-Id: I9dbf84e1531ed3e32b21c8fec13266cfe4d23cd3

--- common.mk
+++ common.mk
@@ -7,5 +7,11 @@
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Update Engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
