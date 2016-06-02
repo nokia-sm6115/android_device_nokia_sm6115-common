4ce9cb9fe77b78beb3123649f0798371f122ac61
Andreas Gampe <agampe@google.com>
2016-06-02 10:05:12 -0700

    sm6115-common: Pull in Otapreopt
    Add otapreopt packages to marlin and sailfish. This prepares
    for A/B OTA dexopting.
    
    Bug: 25612095
    Change-Id: I4fcf64fdb9933f3a3a52fce76264e352f6e774f6

--- common.mk
+++ common.mk
@@ -7,6 +7,16 @@
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
 # Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
