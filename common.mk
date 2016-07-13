b16e04d058c0f74aba2422af7ead4b38b0d0917a
Glenn Kasten <gkasten@google.com>
2016-07-13 13:54:40 -0700

    sm6115-common: Add pro audio feature
    Bug: 30115780
    Change-Id: Id272b935db0e91d3354c666811a3c1c6a62a0be3

--- common.mk
+++ common.mk
@@ -79,7 +79,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
