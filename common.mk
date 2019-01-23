82d8801c2581ebb996fa5afb20595b2a7bc694de
Brad Ebinger <breadley@google.com>
2019-01-23 14:59:46 -0800

    sm6115-common: Declare device supports IMS (IP Multimedia Subsystem)
    Change-Id: Iee916f2ef2a537965aaa6b0cbb9a4e0930d81790

--- common.mk
+++ common.mk
@@ -356,6 +356,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
