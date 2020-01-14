cd832652446c68bb895c8ce218e03a8d91620e1e
Leo Liou <leoliou@google.com>
2020-01-14 17:59:51 +0800

    sm6115-common: Inherit the virtual a/b config
    For devices launching with virtual A/B, inherit the virtual A/B
    device base configuration.
    
    Bug: 146466421
    Bug: 149155051
    Test: boot to home, check system property of virtual_ab
    Test: Run full storage-qa test
    Change-Id: I8ebbbe897dc9278f43e37a6a9599b89d68419b27
    Signed-off-by: Leo Liou <leoliou@google.com>

--- common.mk
+++ common.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
