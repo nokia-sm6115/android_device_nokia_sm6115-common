c332c33bd9656a887a809cac4842af82c1e8cda3
LuK1337 <priv.luk@gmail.com>
2021-01-05 13:42:00 +0100

    sm6115-common: Switch to QTI bootctrl HAL
    Change-Id: Ie5079fd4c40cb1f7414f2cba9f98d350f03047d8

--- common.mk
+++ common.mk
@@ -24,6 +24,12 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Boot Control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
+
 # Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
