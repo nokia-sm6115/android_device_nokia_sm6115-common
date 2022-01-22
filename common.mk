e1a5127b1d9a78d124e351c721e147a52af0f181
Alexander Koskovich <akoskovich@pm.me>
2022-01-22 00:46:40 -0700

    sm6115-common: Update telephony package list to LA.QSSI.12.0.r1-05600-qssi.0
    Change-Id: Id6fdabd9d5e0df6d23a982e0e39e06a8acff67ef

--- common.mk
+++ common.mk
@@ -286,9 +286,17 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Telephony
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
+    qti-telephony-hidl-wrapper-prd \
     qti_telephony_hidl_wrapper.xml \
+    qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml
+    qti-telephony-utils-prd \
+    qti_telephony_utils.xml \
+    qti_telephony_utils_prd.xml
