fb20b3f8e52222157259d69f4c861deafc62bdda
Edwin Moquete <edwinmmoquete@gmail.com>
2022-03-04 14:35:53 -0400

    sm6115-common: Build vendor.qti.hardware.perf@2.2
    Change-Id: I587149a352699f53bcb57f367b8518c14773747f

--- common.mk
+++ common.mk
@@ -218,7 +218,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # Update Engine
 PRODUCT_PACKAGES += \
