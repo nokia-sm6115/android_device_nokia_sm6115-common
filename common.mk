4deb368455385e60d4e33200757c2e05ced1810f
Edwin Moquete <edwinmmoquete@gmail.com>
2022-03-04 14:44:43 -0400

    sm6115-common: Import mediacodec seccomp policy
    Change-Id: I1b6c3f4e737ac120d027f7409020d5ca33a18692

--- common.mk
+++ common.mk
@@ -187,6 +187,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
