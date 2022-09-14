960100c26c82f0f2d0e7b936fe9cf44deecff365
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    sm6115-common: Build AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- common.mk
+++ common.mk
@@ -132,7 +132,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm-service.clearkey
 
 # Update Engine
 PRODUCT_PACKAGES += \
