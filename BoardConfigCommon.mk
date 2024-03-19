81382364959e32370f138eebe746f3316097f102
x0x00044 <zlewchan@icloud.com>
2024-03-19 21:57:59 +0100

    sm6115-common: Import partitions setup
    Change-Id: I4e3e00c3c62bd0b9f1f9f9ed96f0a6719da1baaa

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,6 +8,21 @@ COMMON_PATH := device/nokia/sm6115-common
 
 include build/make/target/board/BoardConfigMainlineCommon.mk
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    odm \
+    product \
+    recovery \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -20,6 +35,31 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a9
+
+# Partitions
+-include vendor/lineage/config/BoardConfigReservedSize.mk
+
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 100663296
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 50323242496
+
+BOARD_NOKIA_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system system_ext vendor
+BOARD_NOKIA_DYNAMIC_PARTITIONS_SIZE := 8585740288
+BOARD_SUPER_PARTITION_GROUPS := nokia_dynamic_partitions
+BOARD_SUPER_PARTITION_SIZE := 8589934592
+
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+TARGET_USERIMAGES_USE_F2FS := true
+
+TARGET_COPY_OUT_ODM := odm
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
