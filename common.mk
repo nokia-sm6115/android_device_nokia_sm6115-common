f339f4273c5246fa08a53fd8097bcb961fd37022
x0x00044 <zlewchan@icloud.com>
2024-03-20 23:26:25 +0100

    sm6115-common: Import rootdir
    Change-Id: Id7bdf86f0361b0135481fd47d46786db06de0ebc

--- common.mk
+++ common.mk
@@ -42,5 +42,39 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.dcvs.sh \
+    init.qti.media.sh \
+    init.qti.qcv.sh \
+    qca6234-service.sh
+
+PRODUCT_PACKAGES += \
+    fstab.default \
+    init.drstrange.rc \
+    init.hq.dfx.common.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.qti.ufs.rc \
+    init.target.rc \
+    init.recovery.qcom.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
